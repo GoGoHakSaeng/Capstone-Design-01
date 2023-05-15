a8f11c181c00746abc657f795fa13cf3cbcc7a00
Juhwan Bae <106052739+BaeJuh@users.noreply.github.com>
2023-05-15 20:42:20 +0900

    Scripts by Juhwan
    Make Main page, loading page and ingame sub menu

---
+++ Scripts/Scripts/UIButton/MainBtnType.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainBtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public mainButtonType currentType;
+    public Transform ButtonScale;
+    Vector3 defaultScale;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        defaultScale = ButtonScale.localScale;
+    }
+
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case mainButtonType.New:
+                Debug.Log("새게임");
+                SceneLoad.LoadSceneHandler("InGameScene", 0); // 0 새로하기
+                break;
+            case mainButtonType.Continue:
+                Debug.Log("이어하기");
+                SceneLoad.LoadSceneHandler("Play", 1); // 1 불러오기
+                break;
+            case mainButtonType.Quit:
+                Debug.Log("나가기");
+                Application.Quit();
+                break;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ButtonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ButtonScale.localScale = defaultScale;
+    }
+}
